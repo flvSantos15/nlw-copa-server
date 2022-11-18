8c86adf7a8ddb141b43b2b493befc8dd1c926bfd
flvSantos15 <flvsantos300@gmail.com>
2022-11-18 16:02:09 -0300

    style(package.json): added commitlint + husky + commitizen

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
@@ -10,13 +11,20 @@ async function bootstrap() {
     logger: true, // digo que quero o log
   })
 
+  await fastify.register(cors, {
+    origin: true,
+  })
+
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
 
     return { count }
   })
 
-  await fastify.listen({ port: 3333 })
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
+
+// parei no 46:18 já no front end
+// backend deu um erro no npx prisma generate
