00067556688cec194e9764ef5f70264f28baf819
flvSantos15 <flvsantos300@gmail.com>
2022-11-24 00:56:09 -0300

    feat(new routes and db columns): added new routes and db columns with relations
    added count users route + count guesses route + create pool route + relations for db columns

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Jon Doe',
+      email: 'jon.doe@email.com',
+      avatarUrl: 'https://github.com/flvSantos15.png',
+    },
+  })
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
+      code: 'BOL123',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-25T12:00:00.740Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    },
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-28T12:00:00.740Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeaPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+}
+
+main()
